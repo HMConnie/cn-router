b688cb22064141a5ac53b3059d3ccd54b55b85a5
hinge <kang.yang@shanggucai.com>
2018-07-10 10:58:43 +0800

    提交1.0代码

--- CommonRes/src/main/java/com/sgcai/router/common/utils/RouterHub.java
+++ CommonRes/src/main/java/com/sgcai/router/common/utils/RouterHub.java
@@ -10,8 +10,6 @@ public interface RouterHub {
     String GROUP = "/group";//group 模块
     String USER = "/user";//user 模块
 
-    String SERVICE = "/service";//service 服务解耦组件模块
-
     String APP_MAIN_ACTIVITY = APP + "/mainActivity";//MainActivity的路径
     String APP_SPLASH_ACTIVITY = APP + "/SplashActivity";//SplashActivity的路径
     String GROUP_GROUPLIST_ACTIVITY = GROUP + "/GroupListActivity";//GroupListActivity的路径
@@ -19,7 +17,8 @@ public interface RouterHub {
     String USER_LOGIN_ACTIVITY = USER + "/LoginActivity";//LoginActivity的路径
 
 
-    String APP_COMMUNITION_SERVICE = APP + SERVICE + "/app";
+    String SERVICE = "/service";//service 服务解耦组件模块
+    String APP_COMMUNITION_SERVICE = APP + SERVICE + "/app";//注意service的前路径不能相同， 否则编译报错 例如:app,group,user
     String GROUP_COMMUNITION_SERVICE = GROUP + SERVICE + "/group";
     String USER_COMMUNITION_SERVICE = USER + SERVICE + "/user";
 
